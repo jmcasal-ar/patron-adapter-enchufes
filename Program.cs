903f703045e2f2ccdcaa436e4ffe4defea57402f
Juan <casal.juanmanuel@gmail.com>
2020-10-26 16:26:51 -0300

    Iniciando Proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatronAdapterEnchufes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Patrón Adapter - .Net Core 
+
+Adaptando un taladro que utiliza enchufe de tipo inglés a una ficha europea
